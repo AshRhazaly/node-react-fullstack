5bf39a4e0cf3d7a9e932c16d61e98645a546d8b0
Ash Rhazaly <ashleighrhazaly@gmail.com>
2018-12-08 11:15:50 +0800

    change nigga

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "nigga" });
+  res.send({ hi: "nigga please" });
 });
 
 const PORT = process.env.PORT || 5000;
